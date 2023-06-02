e6584e72b4687eaf0b134b288c9d4126aa5ef728
unknown <alirezaabdolmaleki2278@gmail.com>
2023-06-02 15:58:52 +0330

    change file class User

--- my_class/class_user.py
+++ my_class/class_user.py
@@ -15,7 +15,21 @@ class User:
     def __str__(self):
         return self.user_name
 
+
+class Seller(User):
+    def order(self,order:"order")->None:
+        print(f"from your products{order} was sold!")
+
+
 def main() -> None:
+    ali = User("ali","gmail","8585")
+    reza = Seller("reza","email","9595")
+    sasan = User("sasan","gmail2","7575")
+    ahmad = Seller("ahmad","email2","1000")
+    print("user:",ali.user_name,ali.email)
+    print("saler:",reza.user_name, reza.email)
+    reza.order("book")
+
 
 
 if __name__ == "__main__":
