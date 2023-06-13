718433a826253208de34487d17d3441875085a5e
unknown <alirezaabdolmaleki2278@gmail.com>
2023-06-13 16:33:19 +0330

    1003

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 class Shape:
     def __init__(self, **kwargs):
+        self.area = None
+        self.environment = None
         for key, value in kwargs.items():
             setattr(self, key, value)
+    def Calculate_area(self):
+        pass
 
+    def Calculate_environment(self):
+        pass
+    def show(self):
+        info = ""
+        for key, value in self.__dict__.items():
+            if value>0:
+                info += f"{key}:{value:.2f}\n"
+            print(info)
+    def __str__(self):
+        return self.__class__.__name__
 
-s = Shape(a=5, b=6)
-print(s.a)
+class
