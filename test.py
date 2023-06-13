3a9b25c0cc83db84aa7c6fe9eb4ca423895cc57a
alireza <alirezaabdolmaleki2278@gmail.com>
2023-06-13 12:21:06 +0330

    1002

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-s = "reza"
-t = (1, 2)
-l  = [3, 4]
-print(len(t))
+class Shape:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+s = Shape(a=5, b=6)
+print(s.a)
