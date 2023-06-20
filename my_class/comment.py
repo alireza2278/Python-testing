9aeb925185a09905920efb4b7d6834cdc9c77cb8
unknown <alirezaabdolmaleki2278@gmail.com>
2023-06-20 23:11:05 +0330

    1007

--- my_class/comment.py
+++ my_class/comment.py
@@ -1,13 +1,17 @@
 from datetime import datetime
-from pprint import pprint
 class Product:
     def __init__(self, product_name, price, off):
         self.product_name = product_name
         self.price = price
         self.off = off
 
+    def __str__(self):
+        return self.product_name
+
 
 class Comment:
+    website_name = "sabzlearn.ir"
+
     def __init__(self, product, name, description, like_count, dislike_count):
         self.product = product
         self.name = name
@@ -17,8 +21,16 @@ class Comment:
         self.dislike = dislike_count
 
     def show(self):
-        pprint(f"product: {self.product}\n"
+        print(f"product: {self.product}\n"
                f"name: {self.name}\n"
                f"description: {self.description}\n"
                f"date: {self.date}\n"
                f"like: {self.like} and dislike: {self.dislike}" )
+    @classmethod
+    def info(cls):
+        print(f"website name: {cls.website_name}")
+
+python_course = Product("python expert", 0, 0)
+c1 = Comment(python_course, "reza", "دوره تخمی و طولانی", 50, 7)
+c1.show()
+Comment.info()
