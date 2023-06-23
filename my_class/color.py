e095d1825b7207da6132f9d868a673d65cef940a
alireza <alirezaabdolmaleki2278@gmail.com>
2023-06-23 11:50:53 +0330

    1010

--- my_class/color.py
+++ my_class/color.py
@@ -1,33 +1,33 @@
 class Color:
     def __init__(self, rgb, name):
-        self._rgb = rgb
-        self._name = name
+        pass
+        self.rgb = rgb
+        self.name = name
 
-    def set_name(self, name):
+    def _set_name(self, name):
         if name:
             self._name = name
         else:
             raise ValueError(f"Invalid name {name!r}")
-    def get_name(self):
+
+    def _get_name(self):
         return self._name
 
-    def set_rgb(self, rgb):
+    def _set_rgb(self, rgb):
         self._rgb = rgb
 
-    def get_rgb(self):
+    def _get_rgb(self):
         return self._rgb
 
+    def _del_name(self):
+        print("Deleting...")
+        del self._name
+    name = property()
+    #name = property(_get_name, _set_name, _del_name, "new doc")
 
-c1 = Color(0x6783F5, "light blue")
-print(c1.get_name())
-c1.set_rgb(0x6783F6)
-c1.set_name("blue")
-print(c1.get_rgb())
 
-print("*"*40)
+c = Color(0x45548, "blou")
+c.name = "red"
+print(c.name)
+help(c)
 
-c2 = Color(0x6784F5, "light red")
-print(c2.get_name())
-c2.set_rgb(0x6763F6)
-c2.set_name("red")
-print(c2.get_rgb())
