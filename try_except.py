4da33fd4d01557a10ce33a9e92f74442be319bf5
alireza <alirezaabdolmaleki2278@gmail.com>
2023-06-30 19:54:42 +0330

    hg

--- try_except.py
+++ try_except.py
@@ -12,3 +12,55 @@ except Exception as Ex:
     print(Ex.__class__.__name__)
 
 print("end")
+#----------------------------------------
+
+
+def div(q, r, z):
+    try:
+        f = q + r
+        result = f / z
+
+# مدریت خطا برای تایپ ارور
+    except TypeError as Ty:
+        print(Ty.__class__.__name__)
+
+# مدریت خطا برای مخرج صفر تقسیم
+    except ZeroDivisionError as zde:
+        print(zde.__class__.__name__)
+
+# زمانی اجرا میشود ک ترای بدون هیچ خطا انجام شود
+    else:
+        print(result)
+#  صد در اجرا میشود در هر حالت حتی با وجود خطا یا ریترن در قسمت ترای و ریترن ترای حذف میشود
+    finally:
+        print("Ok")
+
+
+def func():
+    try:
+        a = int(input("a: "))
+        b = int(input("b: "))
+        c = int(input("c: "))
+        div(a, b, c)
+    except ValueError as va:
+        print("**", va.__class__.__name__)
+# اگر تابع دیو مدریت خطا مخرج صفر نداشته باشد میآید این تابع را نیز برای مدریت خطا برسی میکند
+    except ZeroDivisionError:
+        print("Error")
+
+
+func()
+#-------------------------------
+
+
+#ایجاد استثنا دستی , (raise)کردن استثنا
+def sol(x, y, z):
+    if z == 0:
+        raise ZeroDivisionError("No Enter zero")
+    s = x+y
+    d = s / z
+    print(d)
+
+
+sol(4, 2, 0)
+
