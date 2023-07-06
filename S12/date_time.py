2b6009a44f82cbf19552fef5ece0e24961e40439
alireza <alirezaabdolmaleki2278@gmail.com>
2023-07-06 13:44:15 +0330

    end season 12

---
+++ S12/date_time.py
@@ -0,0 +1,14 @@
+# date: (from datetime import date), d = date(2023, 12, 11) or d = date.today() :نمایش تایخ
+
+
+# time: (from datetime import time), t = time(12, 11, 6) -> print(t.minute) or print(d.hour) :نمایش زمان
+
+
+# datetime: (import datetime), d = datetime.datetime(2023, 12, 11, 22, 12, 56) or d = datetime.datetime.now() -> print(d) and print(d.second) : نمایش زمان و تاریخ
+
+
+# timedelta: d1 = datetime.datetime.now() and d2 = datetime.datetime(2022, 4, 29, 12, 13, 2) -> print(d1-d2) :اختلاف دو زمان
+
+
+# strftime: (import datetime), now = datetime.datetime.now() -> print(now.strftime("%Y/%b/%d-%H:%M")) :شخصی سازی نمایش(در سایت برای شخصی سازی مقادیر موجود است.)
+
